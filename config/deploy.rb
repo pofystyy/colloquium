2e77639ec13b994f63c26592f315a5247b17be7f
pofystyy <pofystyy@gmail.com>
2019-09-26 12:59:11 +0300

    added rvm_ruby_version to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :use_sudo,                true
 set :migration_role,          :app
 set :assets_roles,            [:web, :app]
 set :rails_env,               :production
+set :rvm_ruby_version,        'ruby-2.3.6'
 
 # Puma settings
 set :puma_threads,            [0, 8]
