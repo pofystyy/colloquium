15e81210f937ab93431a6eb4ff871a1cb0d89338
pofystyy <pofystyy@gmail.com>
2019-09-26 10:33:54 +0300

    hotfix

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repo_url, 'git@github.com:pofystyy/colloquium.git'
 set :branch, :master
 set :deploy_to, '/home/deploy/app'
 set :pty, true
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml config/master.key}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
